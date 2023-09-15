9502050d29b28e8c37a15dd744ddd6562da289df
terezpohankova <terezpohankova@gmail.com>
2023-09-15 08:48:48 +0200

    sebs_ETdaily

--- valencia_fcs.py
+++ valencia_fcs.py
@@ -1,12 +1,18 @@
-from osgeo import gdal
-from affine import Affine
-import rasterio as rio
-import numpy as np
-import os, sys
-from metpy.units import units
-from metpy.units import units
+import os
+import sys
 
-from metpy.calc import specific_humidity_from_mixing_ratio, saturation_mixing_ratio
+import numpy as np
+import pint
+import rasterio as rio
+from affine import Affine
+from metpy.units import units
+from osgeo import gdal
+
+ureg = pint.UnitRegistry()
+Q_ = ureg.Quantity
+from metpy.calc import (saturation_mixing_ratio,
+                        specific_humidity_from_mixing_ratio, vapor_pressure)
+
 
 def saveimg(image_georef, img_new, outputPath, epsg):
     step1 = gdal.Open(image_georef, gdal.GA_ReadOnly) 
@@ -34,22 +40,109 @@ def fvc(ndvi):
     fvc = ndvi - 0.05
     return fvc
 
-def saturation_vapor_pressure(sat_pressure_0c, temperature):
-    satVapPress = sat_pressure_0c * np.exp(17.67 * (temperature - units.Quantity(273.15, 'kelvin')) / (temperature - units.Quantity(29.65, 'kelvin')))
-    
+def e0(es_t_wet, t_wet, t_dry, pressure):
+    #e0 = (0.000662*pressure) * 2.7183 ** (17.27 * temperature / (temperature + 237.3))
+    e0 = es_t_wet - 0.000662 * (pressure) * (t_dry - t_wet) 
+    return e0
+
+def es(temperature):
+    #satVapPress = (e0_temp_max + e0_temp_min) / 2
+    satVapPress = 0.6108 * (2.7183 ** ((17.27 * temperature) / (temperature + 237.3)))
     return satVapPress
 
-def psychrometric_vapor_pressure_wet(satvappres, my_press_ref, tair_dry4, my_t_ref_unit, psychrometer_coefficient = None):
+def dry_air_pressure(pressure, water_vapour):
+    dry_air_press = pressure - water_vapour
+    return dry_air_press
+
+def es_slope(temperature):
+    slopevappress =  (4098 * (0.6108 * 2.7183 ** ((17.27 * temperature) / (temperature + 237.3)))/(temperature + 237.3) ** 2)
+    return slopevappress
+
+def psychrometric_vapor_pressure_wet(satvappres, my_press_ref, tair_dry4, my_t_ref, psychrometer_coefficient = None):
     if psychrometer_coefficient is None:
-        psychrometer_coefficient = units.Quantity(6.21e-4, '1/K')
-        return satvappres - psychrometer_coefficient * my_press_ref * (tair_dry4 - my_t_ref_unit).to('kelvin')
+        psychrometer_coefficient = 6.21e-4
+        return satvappres - psychrometer_coefficient *((my_press_ref / 1000)) *(tair_dry4 - my_t_ref)
+
+def relative_humidity(sat_vap_press, vapor_pressure):
+    RH = 100 * (sat_vap_press / vapor_pressure)
+    return RH
+
+def dewpoint(temperature, rel_humidity):
+    # Magnus-Tetens formula
+    # 17.27 - Magnus coefficient
+    # 243.5 - Magnus Coefficient -> https://journals.ametsoc.org/view/journals/apme/35/4/1520-0450_1996_035_0601_imfaos_2_0_co_2.xml
+    dew = temperature - ((100 - rel_humidity)/5.)
+    return  dew
+
+#specific humidity
+def SH(pressure, vapor_pressure):
+    spec_hum = (.622 * pressure / (pressure - vapor_pressure) * 100)
+    return spec_hum
+
+def SWnet(SWIN, albedo):
+    swnet = (1 - albedo) * SWIN
+    return swnet
+
+def LWnet(lse, LWIN, B_constant, LST):
+    lwnet = (lse * LWIN) - (lse * B_constant * (LST ** 4))
+    return lwnet
+
+def G(Rn, fvc):
+    G0 = Rn * (0.05 + (1 - fvc) * (0.315 - 0.05))
+    return G0
+
+def airdenstiy(dry_air_press, gas_constant, temperature_K, water_vapour):
+    airdens =  dry_air_press / (gas_constant * temperature_K) + (water_vapour / (461.495  * temperature_K)) 
+    return airdens
+
+#psychrometric const
+def gamma(press, Cp, l_lambda, e):
+    psychro = (Cp  * press) / (e * l_lambda)
+    return psychro
+
+# zero plane displacement
+def d(cropheight):
+    d_hc = 0.667 * cropheight
+    return d_hc
+
+# friction velocity, # https://inis.iaea.org/collection/NCLCollectionStore/_Public/37/118/37118528.pdf
+def u_friction(wind_sp, karmann, measurement_height, d, z0m):
+    u_ = (wind_sp * karmann) / np.log((measurement_height-d)/z0m) 
+    return u_
+
+def u_pbl(wind_sp, height_measure, z0m, z_pbl, zero_d):  # wind speed, height of wind speed measurement, z0m, z_pbl
+    """Calculates Planetary Boundary Layer wind speed [m s-1] from Fcover"""
     
-def dewpoint(my_ea_ref, sat_pressure_0c):
-    val = np.log(my_ea_ref / sat_pressure_0c)
-    return  (units.Quantity(0., 'degC') + units.Quantity(243.5, 'delta_degC') * val / (17.67 - val))
+    u_c = np.log((z_pbl - zero_d) / z0m) / np.log((height_measure - zero_d) / z0m)
+    upbl = wind_sp * u_c
+    u_pbl = np.where(upbl < 0, 0,upbl) 
+    return u_pbl
+
+def PSIh_y(Y, psy_c, psy_d, psy_n):
+        # constants (Brutsaert, 1999)
+        Y = abs(Y)
+        PSIh_y = (1.0 - psy_d) / psy_n * np.log((psy_c + Y ** psy_n) / psy_c)
+        return PSIh_y
 
 
-def specific_humidity_from_dewpoint(pressure, dewpoint):
-    mixing_ratio = saturation_mixing_ratio(pressure, dewpoint)
-    spedicif_hum_dew = specific_humidity_from_mixing_ratio(mixing_ratio)
-    return spedicif_hum_dew
+def Cw(alfa, psi_C0, psi_C1, psi_C11, C21, psi_C22, z0m):
+
+    ### creating conditional mask
+       
+    Mask = np.copy(z0m)
+    Mask = np.where(Mask <= psi_C0, -9999.0, np.where(Mask != -9999.0, 0, np.where(Mask == -9999.0, 1, Mask)))
+
+    C_1 = np.log(alfa) + psi_C11 - psi_C1
+    C_2 = np.log(C21) + psi_C22 - psi_C1
+    C_1_cond = np.where(C_1 == -np.inf, 0, np.where(C_1 == np.inf, 0, np.where(C_1 == None, np.nan_to_num(C_1 * Mask), C_1)))
+    C_2_cond = np.where(C_2 == -np.inf, 0, np.where(C_2 == np.inf, 0, np.where(C_2 == None, np.nan_to_num(C_2 * Mask), C_2)))
+
+    # SUM CONDITIONAL
+    C = C_1_cond + C_2_cond
+    C[C < 0.0] = 0
+    return C
+
+def zdh0(zd0, z0m):
+    zdh = np.log(zd0 / (0.1 * z0m))
+
+    return zdh
--- valencia_v2.py
+++ valencia_v2.py
@@ -1,44 +1,75 @@
-import metpy
-from metpy.units import units
+import math
+import os
+import sys
+from pprint import pprint
 
-from metpy.calc import specific_humidity_from_mixing_ratio, saturation_mixing_ratio
+import metpy
 import numpy as np
 #import sebs_function_bank as sfb
 import openpyxl as xl
-import rasterio as rio
-from rasterio.plot import show
-
 import pandas as pd
+import pint
+import rasterio as rio
 import scipy
-import os, sys
+
+from metpy.units import units
+from osgeo import gdal
+from rasterio.plot import show
+from scipy import ndimage
 
 from valencia_fcs import *
 
+ureg = pint.UnitRegistry()
+Q_ = ureg.Quantity
 
 #######################################################################################################
 #######################################################################################################
 #######################################################################################################
-
+# SEBS
 ## define inputs
+#FOLDERS
 INPUT_FOLDER = r'/home/tereza/ownCloud/PhD/staze/valencia2023/work/SEBS/20180718_1155_LST_EMISIVIDAD_ALBEDO_NDVI/'
 OUTPUT_FOLDER = r'/home/tereza/ownCloud/PhD/staze/valencia2023/work/SEBS/20180718_1155_LST_EMISIVIDAD_ALBEDO_NDVI/TEST_OUTPUT/'
+FOLDER_METEO = r'/home/tereza/ownCloud/PhD/staze/valencia2023/work/SEBS/fluxes_Braccagni'
+
+
 NDVI_name = '1155NDVI.gcu'
 LST_name = '1155LST.gcu'
 ALBEDO_name = '1155albedo.gcu'
 LSE_name = '1155LSE.gcu'
 Rnet = '1155_Rnet.gcu'
 Fvc = 'fvc.gcu'
-FOLDER_METEO = r'/home/tereza/ownCloud/PhD/staze/valencia2023/work/SEBS/fluxes_Braccagni'
+
 METEO = 'meteo_cornJUN_JUL1.xlsx'
 
 ## CONSTANTS
 epsg = 'EPSG:32632'
-B_constant = units.Quantity(scipy.constants.sigma, "W/m**2 ")
+B_constant = scipy.constants.sigma # W m^-2 K^-4
+Z = 10 # meter
+Z_m = 2 # m ; height of measurement
+R = 287.05 # J/kg-K, gas constant, https://designbuilder.co.uk/helpv3.4/Content/Calculation_of_Air_Density.htm
+Cp = 0.001013 # specific heat at constant pressure [MJ kg-1 °C-1]
+l_lambda =  2.45 #latent heat of vaporization [MJ kg-1]
+e =  0.622 #ratio molecular weight of water vapour/dry air 
+k = 0.41 # con karmann
+g = 9.81
+z_pbl = 1000 # m, height of planetary boundary layer
 
+a = 1
+b = 0.667
+c = 5.0
+de = 0.35
+
+psy_c = 0.33
+psy_d = 0.057
+psy_n = 0.78
+
+alfa = 0.12
+beta = 125.0
 
 ##open LST raster
 with rio.open(os.path.join(INPUT_FOLDER, LST_name)) as lst_raster:
-    lst_b1 = lst_raster.read(1, masked = False)
+    lst_b1 = lst_raster.read(1, masked = False) # Kelvin
     profile = lst_raster.profile
 
 ## open NDVI
@@ -48,7 +79,7 @@ with rio.open(os.path.join(INPUT_FOLDER, NDVI_name)) as ndvi_raster:
 
 ## open emissivity
 with rio.open(os.path.join(INPUT_FOLDER, LSE_name)) as emissivity_raster:
-    lse_b1 = emissivity_raster.read(1, masked = False)
+    lse_b1 = emissivity_raster.read(1, masked = False) # "W/m^2"
     profile = emissivity_raster.profile
 
 ## open albedo
@@ -57,38 +88,88 @@ with rio.open(os.path.join(INPUT_FOLDER, ALBEDO_name)) as albedo_raster:
     profile = albedo_raster.profile
 
 # open meteorology
-wb = pd.read_excel(os.path.join(FOLDER_METEO, 'meteo_cornJUN_JUL1.xlsx'), sheet_name='meteo_cornJUN_JUL')
-#print(wb.head())
+rows_to_read = [0,13681]
+## 0 - dry bulb     2 - wind        4 - SWIN
+## 1 - wet bulb     3 - pressure    5- LWIN
+#skiprows = lambda x: x not in rows_to_read
+meteo_df = pd.read_excel(os.path.join(FOLDER_METEO, 'meteo_cornJUN_JUL1.xlsx'), sheet_name='meteo_cornJUN_JUL', 
+                   usecols = ['TIMESTAMP', 'Tair_dry4', 'Tair_wet4', 'wind4', 'pressure', 'SWIN', 'LWIN'], index_col= 'TIMESTAMP')
 
 # get T_AIR_WET
-my_t_ref = pd.to_numeric(wb.iloc[13680]['Tair_wet4'], errors='coerce', downcast='float') #Tair_wet4, 18/7/20218 12:00
-my_t_ref_unit = units.Quantity(my_t_ref, "°C") # specify units
+my_t_ref = meteo_df.iloc[13681,1] # Tair_wet4, 18/7/20218 12:00 # °C
+my_t_ref_K = my_t_ref + 273.15 # K
+pprint(f"Wet Bulb Temperature: {my_t_ref} °C.")
 
 # get T_AIR_DRY
-tair_dry4 = pd.to_numeric(wb.iloc[13680] ['Tair_dry4'], errors='coerce', downcast='float')
-tair_dry4 = units.Quantity(tair_dry4, "°C") # specify units
+tair_dry4 = meteo_df.iloc[13681,0] # °C
+pprint(f"Dry Bulb Temperature: {tair_dry4} °C.")
+tair_dry4_max = meteo_df['Tair_dry4'].max()
+pprint(f"Maximum dry bulb temperature Pressure: {tair_dry4_max} °C.")
+tair_dry4_min = meteo_df['Tair_dry4'].min()
+pprint(f"Minimum dry bulb temperature Pressure: {tair_dry4_min} °C.")
+
+#tair_dry4 = Q_(tair_dry4, "degC") # specify units
 
 # get W
-my_w_ref = pd.to_numeric(wb.iloc[13680]['wind4'], errors='coerce', downcast='float') #questo e il valore che mi serve, wind4 alle 12 del 18.07.2018 (m/s)
-my_w_ref = units.Quantity(my_w_ref, "m/s")
+my_w_ref = meteo_df.iloc[13681,2] # m/s
+pprint(f"Wind Speed: {my_w_ref} m/s.")
 
 # Pressure
-my_press_ref = 100858.1117 #Pa, valure ottenuto da ERA5
-my_press_ref = units.Quantity(my_press_ref, "Pa")
-my_press_surf = pd.to_numeric(wb.iloc[13680]['pressure']) 
-my_press_surf = units.Quantity(my_press_surf, "hPa")
+my_press_ref = 100858.1117 # Pa
+my_press_ref_kpa = my_press_ref / 1000
+#pprint(f"Surface Pressure: , {my_press_ref} Pa.")
 
+my_press_surf = meteo_df.iloc[13681,3] # kPa
+pprint(f"Surface Pressure: {my_press_surf} kPa.") # ???? UNITS
+
+#my_press_surf = Q_(my_press_surf, "kPa")
+press_surf_array = np.full_like(lst_b1, my_press_surf)
 
 # create array in shape of LST with values of Tair_wet4
 t_ref_array = np.full_like(lst_b1, my_t_ref)
 w_ref_array = np.full_like(lst_b1, my_w_ref)
-press_ref_array = units.Quantity(np.full_like(lst_b1, my_press_ref), 'Pa')
+press_ref_array = np.full_like(lst_b1, my_press_ref) # Pa
 
 # pressure ### WHICH UNITS???
-sat_pressure_0c = units.Quantity(611, 'Pa')
-sat_vap_press = units.Quantity(saturation_vapor_pressure(sat_pressure_0c, my_t_ref_unit), 'Pa')
-my_ea_ref = units.Quantity(psychrometric_vapor_pressure_wet(sat_vap_press, my_press_ref, tair_dry4, my_t_ref_unit), 'Pa')
+sat_pressure_0c = 611.2 # Pa
 
+sat_vap_press = es(tair_dry4) # kPa
+pprint(pprint(f"Saturated Vapour Pressure: {sat_vap_press} kPa."))
+
+slope_vap_press =  es_slope(my_t_ref)  # kPa/°C
+pprint(f"Slope of saturation vapour pressure curve : {slope_vap_press} kPa/°C.")
+
+my_ea_ref = psychrometric_vapor_pressure_wet(sat_vap_press, my_press_ref_kpa, tair_dry4, my_t_ref)
+my_ea_ref_array = np.full_like(lst_b1, my_ea_ref) # Pa
+saveimg(os.path.join(INPUT_FOLDER, LST_name), my_ea_ref_array, os.path.join(OUTPUT_FOLDER, 'psychro_wet_1155.GCU'), 'EPSG:32632')
+pprint(f"Wet Psychrometric Vapour Pressure: {my_ea_ref} kPa.")
+
+# Warter vapour pressure
+water_vapour = e0(es(my_t_ref), my_t_ref, tair_dry4, my_press_ref_kpa) # kPa
+pprint(f"Actual Water Vapour pressure: {water_vapour} kPa.")
+
+# Humidity
+RH = relative_humidity(sat_vap_press, water_vapour) # %
+pprint(f"Relative Humidity: {RH} %.")
+#dew point
+dew = dewpoint(my_t_ref, RH) # °C
+pprint(f"Dewpoint temperature: {dew} °C.")
+# specific humidity
+my_q_ref = SH(my_press_ref_kpa, water_vapour) # g/kg
+pprint(f"Specific humidity: {my_q_ref} %.")
+q_ref_array = np.full_like(lst_b1, my_q_ref)
+
+# air density
+pressure_dry_air = dry_air_pressure(my_press_ref, water_vapour) # kPa
+air_density = airdenstiy(pressure_dry_air, R, my_t_ref_K, water_vapour) # kg/m3
+pprint(f"Air density: {air_density} kg/m3.")
+
+## Z array
+z_ref_array = np.full_like(lst_b1, Z)
+
+#psychrometric
+gamma_cons = gamma(my_press_ref_kpa, Cp, l_lambda, e) # kPa °C-1
+print(pprint(f"Psychrometric constant: {gamma_cons} kPa °C-1."))
 
 ## CALCULATIONS
 # FVC
@@ -108,31 +189,90 @@ saveimg(os.path.join(INPUT_FOLDER, LST_name), my_z0m, os.path.join(OUTPUT_FOLDER
 my_hc = my_z0m / 0.136 #Brutsaert, 1982
 saveimg(os.path.join(INPUT_FOLDER, LST_name), my_hc, os.path.join(OUTPUT_FOLDER, 'hc_1155.GCU'), 'EPSG:32632')
 
-# dew point
-dew = dewpoint(my_ea_ref, sat_pressure_0c)
-#print(dew)
+## radiation
+my_SWin = meteo_df.iloc[13681,4] # Downwelling shortwave radiation, W/m2
+pprint(f"Downwelling shortwave radiation: {my_SWin} W/m2.")
+my_LWin = meteo_df.iloc[13681,5] # Downwelling longwave radiation, W/m2
+pprint(f"Downwelling longwave radiation: {my_LWin} W/m2.")
 
-psychro = psychrometric_vapor_pressure_wet(sat_vap_press, press_ref_array, tair_dry4, my_t_ref_unit) #kolik cca by to mělo vyjít?
-saveimg(os.path.join(INPUT_FOLDER, LST_name), psychro, os.path.join(OUTPUT_FOLDER, 'psychro_1155.GCU'), 'EPSG:32632')
-#print(psychro)
-
-# specific humidity
-my_q_ref = specific_humidity_from_dewpoint(my_press_ref, dew)
-my_q_ref = units.Quantity(my_q_ref.magnitude, "g/kg")
-q_ref_array = np.full_like(lst_b1, my_q_ref)
-
-## TEZKO RICT CO TO JE
-my_SWin = pd.to_numeric(wb.iloc[13680]['SWIN'], errors='coerce', downcast='float') 
-my_SWin = units.Quantity(my_SWin, "m/s")
-my_LWin = pd.to_numeric(wb.iloc[13680]['LWIN'], errors='coerce', downcast='float') 
-my_LWin = units.Quantity(my_LWin, "m/s")
-
-my_SWnet = (1 - albedo_b1) * my_SWin
+my_SWnet = SWnet(my_SWin, albedo_b1) # W/m2
 saveimg(os.path.join(INPUT_FOLDER, LST_name), my_SWnet, os.path.join(OUTPUT_FOLDER, 'my_SWnet_1155.GCU'), 'EPSG:32632')
-
-my_LWnet = (lse_b1 * my_LWin) - (lse_b1 * B_constant * (lst_b1 ** 4))
+my_LWnet = LWnet(lse_b1, my_LWin, B_constant, lst_b1) # W/m2
 saveimg(os.path.join(INPUT_FOLDER, LST_name), my_LWnet, os.path.join(OUTPUT_FOLDER, 'my_LWnet_1155.GCU'), 'EPSG:32632')
-sys.exit()
+
+my_Rn = my_SWnet + my_LWnet # W/m2
+saveimg(os.path.join(INPUT_FOLDER, LST_name), my_Rn, os.path.join(OUTPUT_FOLDER, 'my_Rn_1155.GCU'), 'EPSG:32632')
+
+my_G = G(my_Rn, my_fvc)
+saveimg(os.path.join(INPUT_FOLDER, LST_name), my_G, os.path.join(OUTPUT_FOLDER, 'my_G_1155.GCU'), 'EPSG:32632')
+
+zero_except = np.seterr(all = "ignore")
+
+zero_d = d(my_hc) # m
+saveimg(os.path.join(INPUT_FOLDER, LST_name), zero_d, os.path.join(OUTPUT_FOLDER, 'd_1155.GCU'), 'EPSG:32632')
+
+u_ = u_friction(my_w_ref, k, Z_m, zero_d, my_z0m) # m/s
+u_ = np.where(u_ < 0, np.mean(u_), u_ )
+saveimg(os.path.join(INPUT_FOLDER, LST_name), u_, os.path.join(OUTPUT_FOLDER, 'u_friction_1155.GCU'), 'EPSG:32632')
+
+u_planet = u_pbl(my_w_ref, 2, my_z0m, 1000, zero_d) # m/s
+saveimg(os.path.join(INPUT_FOLDER, LST_name), u_planet, os.path.join(OUTPUT_FOLDER, 'u_planet_1155.GCU'), 'EPSG:32632')
+
+zd0 = z_pbl - zero_d
+zdh = zdh0(zd0, my_z0m)
+zdm = np.log(zd0 / my_z0m)
+ku = 0.41 * u_planet
+RUstar = ku / zdm
+saveimg(os.path.join(INPUT_FOLDER, LST_name), RUstar, os.path.join(OUTPUT_FOLDER, 'RUstar_1155.GCU'), 'EPSG:32632')
+
+L_w = (RUstar ** 3.0) * air_density / (0.61 * k * g * (my_Rn - my_G) / 2450000)
+L_w = np.where(L_w > 3000, 3000, L_w)
+saveimg(os.path.join(INPUT_FOLDER, LST_name), L_w, os.path.join(OUTPUT_FOLDER, 'L_w2_1155.GCU'), 'EPSG:32632')
+#print(np.nanmin(RUstar))
+
+dzeta = u_planet / L_w
 
 
+psi_m_stab = -(a * dzeta + b * (dzeta - c / de) * np.exp((-de) * dzeta) + b * c / de)
+psi_h_stab = -((1 + 2 * a * dzeta / 3) ** 1.5 + b* (dzeta - c / de) * np.exp((-de) * dzeta)+ (b * c/de - 1))
+
+ra = (np.log((Z - zero_d) / my_z0m) - psi_m_stab) * (np.log((z_pbl - zero_d)/(0.1*my_z0m)) - psi_h_stab) / (k ** 2 * my_w_ref) # s/m
+saveimg(os.path.join(INPUT_FOLDER, LST_name), ra, os.path.join(OUTPUT_FOLDER, 'ra_1155.GCU'), 'EPSG:32632')
+
+    
+# conditional masks?
+C0 = (alfa / beta) * z_pbl
+C1 = np.nanmin(my_z0m * 0.1) / L_w
+C11 = -alfa * z_pbl / L_w
+C21 = z_pbl / (beta * my_z0m)
+C22 = -beta * my_z0m / L_w
+
+psi_C0 = PSIh_y(C0, psy_c, psy_d, psy_n)
+psi_C1 = PSIh_y(C1, psy_c, psy_d, psy_n)
+psi_C11 = PSIh_y(C11, psy_c, psy_d, psy_n)
+psi_C22 = PSIh_y(C22, psy_c, psy_d, psy_n)
+
+C_wet2 = np.nanmin(psi_h_stab)
+C_wet1 = Cw(alfa, psi_C0, psi_C1, psi_C11, C21, psi_C22, my_z0m)
+C_wet = C_wet1 + C_wet2
+
+re_w = (zdh - C_wet) / (k * RUstar)
+re_w1 = zdh / (k * RUstar)
+re_w2 = re_w
+rew = re_w1 + re_w2
+flux_H = (my_Rn - (air_density / rew) * ((water_vapour - sat_vap_press) / gamma_cons)) / (1.0 + slope_vap_press / gamma_cons)
+saveimg(os.path.join(INPUT_FOLDER, LST_name), flux_H, os.path.join(OUTPUT_FOLDER, 'H_1155.GCU'), 'EPSG:32632')
+
+ET_Instant = my_Rn - flux_H - my_G
+saveimg(os.path.join(INPUT_FOLDER, LST_name), ET_Instant, os.path.join(OUTPUT_FOLDER, 'ET_1155.GCU'), 'EPSG:32632')
+
+
+T = ((lst_b1 - 273.15) + my_t_ref) / 2
+delta = (45.03 + 3.014 * T + 0.05345 * T ** 2 + 0.00224 * T ** 3) * 0.001
+LE_p = (delta * (my_Rn - my_G) + air_density * Cp * (sat_vap_press - water_vapour) / ra) / (delta + gamma_cons) # latent heat flux
+saveimg(os.path.join(INPUT_FOLDER, LST_name), LE_p, os.path.join(OUTPUT_FOLDER, 'LE_1155.GCU'), 'EPSG:32632')
+
+ETR = (my_Rn - my_G - flux_H) / l_lambda # # Actual Evapotranspiration [mm/day]
+ETR = (ETR / (l_lambda * 1000000)) * 0.3 * 86400 # daily ET
+saveimg(os.path.join(INPUT_FOLDER, LST_name), ETR, os.path.join(OUTPUT_FOLDER, 'DailyET_1155.GCU'), 'EPSG:32632')
 
